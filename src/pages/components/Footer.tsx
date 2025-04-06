7e1960a466caadc2b64a82da230b4d2addbfe93e
ramz <ramelopanisjr.06@gmail.com>
2025-04-06 15:09:16 +0800

    Update: adding navigation

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -4,26 +4,28 @@ import { CiFacebook } from "react-icons/ci";
 import { FaTiktok } from "react-icons/fa";
 const Footer = () => {
   return (
-      <footer className="flex items-center p-5 bg-cyan-500 text-white px-40 gap-10 poppins-regular w-full fixed bottom-0">
-        <div className="flex flex-col gap-5 w-full">
-          <p className="font-bold text-2xl">Contact Me</p>
-          <div className="flex flex-col gap-2">
-            <span className="flex items-center gap-2">
-              <label htmlFor="email">Email:</label>
-              <p>ramelopanisjr.06@gmail.com</p>
-            </span>
-            <p>09608757182</p>
-          </div>
+    <footer className="flex items-center p-5 bg-cyan-500 text-white px-40 gap-10 poppins-regular w-full fixed bottom-0">
+      <div className="flex flex-col gap-5 w-full">
+        <p className="font-bold text-2xl">Contact Me</p>
+        <div className="flex flex-col gap-2">
+          <span className="flex items-center gap-2">
+            <label htmlFor="email">Email:</label>
+            <p>ramelopanisjr.06@gmail.com</p>
+          </span>
+          <p>09608757182</p>
         </div>
-        <div className="flex flex-col gap-5">
-          <p className="font-bold text-2xl">Social Media</p>
-          <div className="flex gap-5">
+      </div>
+      <div className="flex flex-col gap-5">
+        <p className="font-bold text-2xl">Social Media</p>
+        <div className="flex gap-5">
+          <a href="https://www.instagram.com/ramz_panisjr/" className="cursor-pointer">
             <FaInstagram className="w-8 h-8" />
-            <CiFacebook className="w-8 h-8" />
-            <FaTiktok className="w-7 h-7" />
-          </div>
+          </a>
+          <CiFacebook className="w-8 h-8" />
+          <FaTiktok className="w-7 h-7" />
         </div>
-      </footer>
+      </div>
+    </footer>
   );
 };
 
