e691f347351f5873ffe04c55ffa554006e3b2a50
ramz <ramelopanisjr.06@gmail.com>
2025-04-06 15:13:16 +0800

    Update: add navigation to fb

--- src/pages/components/Footer.tsx
+++ src/pages/components/Footer.tsx
@@ -4,24 +4,32 @@ import { CiFacebook } from "react-icons/ci";
 import { FaTiktok } from "react-icons/fa";
 const Footer = () => {
   return (
-      <footer className="w-full flex items-center p-5 bg-cyan-500 text-white md:px-40 gap-10 poppins-regular md:flex-nowrap flex-wrap">
-        <div className="flex flex-col gap-5 w-full">
-          <p className="font-bold text-2xl">Contact Me</p>
-          <div className="flex flex-col gap-2">
-            <span className="flex items-center gap-2">
-              <label htmlFor="email">Email:</label>
-              <p>ramelopanisjr.06@gmail.com</p>
-            </span>
-            <p>09608757182</p>
-          </div>
+    <footer className="w-full flex items-center p-5 bg-cyan-500 text-white md:px-40 gap-10 poppins-regular md:flex-nowrap flex-wrap">
+      <div className="flex flex-col gap-5 w-full">
+        <p className="font-bold text-2xl">Contact Me</p>
+        <div className="flex flex-col gap-2">
+          <span className="flex items-center gap-2">
+            <label htmlFor="email">Email:</label>
+            <p>ramelopanisjr.06@gmail.com</p>
+          </span>
+          <p>09608757182</p>
         </div>
+      </div>
       <div className="flex flex-col gap-5">
         <p className="font-bold text-2xl">Social Media</p>
         <div className="flex gap-5">
-          <a href="https://www.instagram.com/ramz_panisjr/" className="cursor-pointer">
+          <a
+            href="https://www.instagram.com/ramz_panisjr/"
+            className="cursor-pointer"
+          >
             <FaInstagram className="w-8 h-8" />
           </a>
-          <CiFacebook className="w-8 h-8" />
+          <a
+            href="https://www.facebook.com/ramel.panis.1"
+            className="cursor-pointer"
+          >
+            <CiFacebook className="w-8 h-8" />
+          </a>
           <FaTiktok className="w-7 h-7" />
         </div>
       </div>
